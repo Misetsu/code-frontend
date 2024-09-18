05bb935214ebcd2fbd82433c9b97db7d51ffb387
Misetsu <nakitamak@gmail.com>
2024-09-19 02:22:46 +0900

    カードテンプレートを追加

---
+++ src/components/Card/index.vue
@@ -0,0 +1,67 @@
+<script lang="ts" setup>
+import BaseChip from '@/components/Base/Chip/index.vue'
+import BaseText from '@/components/Base/Text/index.vue'
+
+type Props = {
+  title: string
+  content: string
+  direction: 'top' | 'bottom'
+  type: 'simple' | 'chip-card'
+  chipContent?: string
+  chipColor?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  type: 'simple',
+  direction: 'top',
+  chipColor: 'primary'
+})
+</script>
+
+<template>
+  <v-sheet elevated="2" class="card-container">
+    <div v-if="direction == 'top'" class="title-row">
+      <BaseText :content="title" type="h6"></BaseText>
+      <BaseChip
+        v-if="type == 'chip-card'"
+        variant="flat"
+        :content="chipContent"
+        :color="chipColor"
+        size="medium"
+      />
+    </div>
+    <div class="content-row">
+      <BaseText :content="content" type="p"></BaseText>
+      <BaseChip
+        v-if="direction == 'bottom' && type == 'chip-card'"
+        variant="flat"
+        :content="chipContent"
+        :color="chipColor"
+        size="medium"
+      />
+    </div>
+    <div v-if="direction == 'bottom'" class="title-row">
+      <BaseText :content="title" type="h6"></BaseText>
+    </div>
+  </v-sheet>
+</template>
+
+<style lang="css" scoped>
+.card-container {
+  border: 1px solid darkgray;
+  border-radius: 16px;
+  padding: 24px;
+}
+
+.title-row {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+
+.content-row {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+</style>
---
+++ src/features/Test/TestCard/index.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import Card from '@/components/Card/index.vue'
+</script>
+
+<template>
+  <div class="chip-container">
+    <Card title="タイトル" content="内容が入ります。" direction="top" type="simple" />
+    <Card title="タイトル" content="内容が入ります。" direction="bottom" type="simple" />
+    <Card
+      title="タイトル"
+      content="内容が入ります。"
+      direction="top"
+      type="chip-card"
+      chip-content="チップ"
+      chip-color="warning"
+    />
+    <Card
+      title="タイトル"
+      content="内容が入ります。"
+      direction="bottom"
+      type="chip-card"
+      chip-content="チップ"
+    />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.chip-container {
+  display: flex;
+  padding: 32px;
+  gap: 16px;
+}
+</style>
