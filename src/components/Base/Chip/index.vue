07676835d5651d84a096b7dd957142e77bee2c12
Misetsu <nakitamak@gmail.com>
2024-09-19 02:22:07 +0900

    チップテンプレートを追加

---
+++ src/components/Base/Chip/index.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+type Props = {
+  variant: 'elevated' | 'flat' | 'outlined' | 'text' | 'plain'
+  color?: string
+  content?: string
+  icon?: string
+  iconDirection?: 'left' | 'right'
+  size: 'small' | 'medium'
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'elevated',
+  size: 'medium'
+})
+</script>
+
+<template>
+  <v-chip v-bind="$attrs" :variant="variant" :color="color" :class="`base-chip-${size}`">
+    <v-icon
+      v-if="iconDirection === 'left'"
+      :style="size == 'small' ? 'font-size: 12px' : 'font-size: 16px'"
+      class="base-chip-icon"
+      :icon="icon"
+    >
+      {{ icon }}
+    </v-icon>
+    <span :style="size == 'small' ? 'font-size: 8px' : 'font-size: 12px'">
+      {{ content }}
+    </span>
+    <v-icon
+      v-if="iconDirection === 'right'"
+      :style="size == 'small' ? 'font-size: 12px' : 'font-size: 16px'"
+      class="base-chip-icon"
+      :icon="icon"
+    >
+      {{ icon }}
+    </v-icon>
+  </v-chip>
+</template>
+
+<style lang="css" scoped>
+.base-chip-small {
+  padding: 4px;
+}
+
+.base-chip-medium {
+  padding: 4px;
+}
+
+.base-chip-icon {
+  margin: 4px;
+}
+</style>
---
+++ src/features/Test/TestChip/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import BaseChip from '@/components/Base/Chip/index.vue'
+</script>
+
+<template>
+  <div class="chip-container">
+    <BaseChip variant="elevated" color="primary" content="チップ" size="medium" />
+    <BaseChip variant="flat" color="secondary" content="チップ" size="medium" />
+    <BaseChip variant="outlined" color="error" content="チップ" size="small" />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.chip-container {
+  display: flex;
+  padding: 32px;
+  gap: 16px;
+}
+</style>
