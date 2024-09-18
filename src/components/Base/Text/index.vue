82772886aae7d531a3b2d27c58235690609dc7e6
Misetsu <nakitamak@gmail.com>
2024-09-19 02:22:33 +0900

    テキストのテンプレートを追加

---
+++ src/components/Base/Text/index.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+type Props = {
+  content: string
+  type: string
+  color?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'p'
+})
+
+var typography: String
+switch (props.type) {
+  case 'h1':
+    typography = 'h1'
+    break
+  case 'h2':
+    typography = 'h2'
+    break
+  case 'h3':
+    typography = 'h3'
+    break
+  case 'h4':
+    typography = 'h4'
+    break
+  case 'h5':
+    typography = 'h5'
+    break
+  case 'h6':
+    typography = 'h6'
+    break
+  default:
+    typography = 'p'
+}
+</script>
+
+<template>
+  <div :class="`basetext-${typography}`">{{ content }}</div>
+</template>
+
+<style lang="css" scoped>
+.basetext-h1 {
+  font-size: 48px;
+  font-weight: 400;
+}
+
+.basetext-h2 {
+  font-size: 40px;
+  font-weight: 400;
+}
+
+.basetext-h3 {
+  font-size: 36px;
+  font-weight: 400;
+}
+
+.basetext-h4 {
+  font-size: 32px;
+  font-weight: 400;
+}
+
+.basetext-h5 {
+  font-size: 28px;
+  font-weight: 400;
+}
+
+.basetext-h6 {
+  font-size: 24px;
+  font-weight: 400;
+}
+
+.basetext-p {
+  font-size: 20px;
+  font-weight: 400;
+}
+</style>
---
+++ src/features/Test/TestText/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import BaseText from '@/components/Base/Text/index.vue'
+</script>
+
+<template>
+  <div class="text-container">
+    <BaseText content="Heading1" type="h1" />
+    <BaseText content="Heading2" type="h2" />
+    <BaseText content="Heading3" type="h3" />
+    <BaseText content="Heading4" type="h4" />
+    <BaseText content="Heading5" type="h5" />
+    <BaseText content="Heading6" type="h6" />
+    <BaseText content="body" type="p" />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.text-container {
+  display: flex;
+  padding: 32px;
+  gap: 16px;
+  align-content: center;
+}
+</style>
