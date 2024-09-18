647e6861e92ac6b3eb51bb7612ef5b0f99ed24cc
Misetsu <nakitamak@gmail.com>
2024-09-19 02:23:00 +0900

    テストページを追加

---
+++ src/features/Test/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import TestButton from '@/features/Test/TestButton/index.vue'
+import TestChip from '@/features/Test/TestChip/index.vue'
+import TestCard from '@/features/Test/TestCard/index.vue'
+import TestText from '@/features/Test/TestText/index.vue'
+</script>
+
+<template>
+  <div>
+    <TestButton></TestButton>
+    <TestChip></TestChip>
+    <TestText></TestText>
+    <TestCard></TestCard>
+  </div>
+</template>
